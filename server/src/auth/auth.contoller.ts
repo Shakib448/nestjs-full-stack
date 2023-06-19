b77e10bad900312ecb6d205928daf7f281f10a97
Shakib448 <shakiba448@gmail.com>
2023-06-19 12:00:31 +0600

    WIP on nestjs with prisma

--- server/src/auth/auth.contoller.ts
+++ server/src/auth/auth.contoller.ts
@@ -1,12 +1,13 @@
 import { AuthService } from './auth.service';
 import { Body, Controller, Post } from '@nestjs/common';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signUp')
-  signUp(@Body() dto: any) {
+  signUp(@Body() dto: AuthDto) {
     console.log({ dto });
     return this.authService.signUp();
   }
