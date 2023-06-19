4beada074cd5b91fce2c5e6bc58e1e900719aa18
Shakib448 <shakiba448@gmail.com>
2023-06-19 13:18:57 +0600

    WIP on nestjs with prisma

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,28 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signUp(dto: AuthDto) {
-    const salt = await bcrypt.genSaltSync(10);
-    const hash = await bcrypt.hashSync(dto.password, salt);
+    try {
+      const salt = await bcrypt.genSaltSync(10);
+      const hash = await bcrypt.hashSync(dto.password, salt);
 
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        password: hash,
-        firstName: dto.firstName,
-        lastName: dto.lastName,
-      },
-    });
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+        },
+      });
 
-    delete user.password;
-
-    return user;
+      delete user.password;
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+      throw error;
+    }
   }
 
   signIn() {
