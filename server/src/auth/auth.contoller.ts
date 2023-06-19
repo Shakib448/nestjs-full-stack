08ee31a3fa60585a8b71913b9666e1757d2aa97a
Shakib448 <shakiba448@gmail.com>
2023-06-19 12:47:11 +0600

    WIP on nestjs with prisma

--- server/src/auth/auth.contoller.ts
+++ server/src/auth/auth.contoller.ts
@@ -8,8 +8,7 @@ export class AuthController {
 
   @Post('signUp')
   signUp(@Body() dto: AuthDto) {
-    console.log({ dto });
-    return this.authService.signUp();
+    return this.authService.signUp(dto);
   }
 
   @Post('signIn')
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signUp() {
-    return { message: "I'm signed up" };
+  async signUp(dto: AuthDto) {
+    const salt = await bcrypt.genSaltSync(10);
+    const hash = await bcrypt.hashSync(dto.password, salt);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: hash,
+        firstName: dto.firstName,
+        lastName: dto.lastName,
+      },
+    });
+
+    delete user.password;
+
+    return user;
   }
 
   signIn() {
