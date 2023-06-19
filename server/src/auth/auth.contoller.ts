c14778b30791d895791fddbf078970f19f3792e3
Shakib448 <shakiba448@gmail.com>
2023-06-19 20:29:22 +0600

    WIP on nestjs with prisma

--- server/src/auth/auth.contoller.ts
+++ server/src/auth/auth.contoller.ts
@@ -1,6 +1,7 @@
 import { AuthService } from './auth.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthDto } from './dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -12,7 +13,16 @@ export class AuthController {
   }
 
   @Post('signIn')
-  signIn(@Body() dto: AuthDto) {
-    return this.authService.signIn(dto);
+  async signIn(@Body() dto: AuthDto, @Res() res: Response) {
+    const { access_token } = await this.authService.signIn(dto);
+
+    res
+      .cookie('Authorization', access_token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'lax',
+        expires: new Date(Date.now() + 1 * 24 * 60 * 1000),
+      })
+      .send({ status: 'ok' });
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   await app.listen(8000);
 }
 bootstrap();
