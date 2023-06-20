133392ba96dd13c85d4884b586e6635104c88b99
Shakib448 <shakiba448@gmail.com>
2023-06-20 08:43:31 +0600

    WIP on nestjs with prisma & cookie

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { EditUserDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch(':id')
+  editUser(@Param('id', ParseIntPipe) id: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(id, dto);
   }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { EditUserDto } from './dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async editUser(id: number, dto: EditUserDto) {
+    const userExists = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!userExists) throw new ForbiddenException('User not found');
+
+    const user = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        ...dto,
+      },
+    });
+
+    delete user.password;
+
+    return user;
+  }
+}
